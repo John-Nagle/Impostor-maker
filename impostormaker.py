9d11a3706c53c4478799c5be7585de96259ab954
John Nagle <nagle@sitetruth.com>
2018-10-26 12:06:04 -0700

    Compute center and base edge

--- impostormaker.py
+++ impostormaker.py
@@ -28,31 +28,29 @@ class ImpostorFace :
     Contains one or more polygons, all coplanar.
     """        
     def __init__(self, context, target, poly) :
-        self.normal = None                  # no normal yet
-        self.polys = [poly]                 # polygons
-        self.vertexids = set()              # empty set
-        self.edgeids = set()                # set of pairs of vertex IDs
-        me = target.data
+        self.normal = None                  # normal in object coords
+        self.vertexids = []                 # vertex indices into 
+        self.baseedge = None                # (vertID, vertID)
+        self.center = None                  # center of face, object coords
+        me = target.data                    # mesh info
         vertices = me.vertices
         if poly.loop_total < 3 :            # can't compute a normal
             raise RuntimeError("A face of \"%s\" has less than 3 vertices." % (target.name,))
         #   We need a normal for the face. Not a graphics normal, a geometric one based on the vertices.
-        #   Get a list of coordinates
-        meloop = me.loops[poly.loop_start : poly.loop_start + poly.loop_total]   # vertices in this loop
+        #   We also need the base edge for the image and the center of the face.
+        baseedgelength = -math.inf          # no base edge length yet
+        meloop = me.loops[poly.loop_start : poly.loop_start + poly.loop_total]   # vertices of this loop
         for loop_index in range(len(meloop)) :
+            #   Get 3 succesive vertices for 2 edges
             vid0 = meloop[loop_index].vertex_index
             vid1 = meloop[(loop_index + 1) % poly.loop_total].vertex_index # next, circularly
             vid2 = meloop[(loop_index + 2) % poly.loop_total].vertex_index # next, circularly
             ####print("    Loop index %d: Vertex indices: %d %d %d" % (loop_index, vid0, vid1, vid2))  
-            #   Get two successive vertices to get an edge ID
-            self.edgeids.add(tuple(sorted((vid0, vid1))))
-            self.vertexids.add(vid0)        # add to set
-            coords = me.vertices[vid0].co
-            ####print("    Old Vertex: %d: (%1.4f,%1.4f,%1.4f)" % (vid0, coords[0],coords[1],coords[2]))
             #   Get two successive edges to get a normal for the face              
             v0 = me.vertices[vid0].co       # get 3 points, wrapping around
             v1 = me.vertices[vid1].co
             v2 = me.vertices[vid2].co
+            self.vertexids.append(vid0)     # save edge index
             print("    Vertex: %d: (%1.4f,%1.4f,%1.4f)" % (vid0, v0[0],v0[1],v0[2]))
             cross = (v1-v0).cross(v2-v1)    # direction of normal
             #### print("   Cross: " + str(cross))
@@ -64,34 +62,38 @@ class ImpostorFace :
                 if self.normal.dot(cross) < 1.0 - NORMALERROR :
                     raise RuntimeError("A face of \"%s\" is not flat." % (target.name,))
             else :
-                self.normal = cross         # we have a face normal
+                self.normal = cross             # we have a face normal
+            #   Find longest, lowest edge. This will be the bottom of the image.
+            edgelength = (v1-v0).length         # length of edge
+            if edgelength > baseedgelength :
+                baseedgelength = edgelength     # new winner
+                self.baseedge = (vid0, vid1)    # save longest edge tuple
+            #   Compute center of face. Just the average of the corners.
+            if not self.center :
+                self.center = v0
+            else :
+                self.center = self.center + v0  # sum the vertices
+        #   All vertices examined.  
+        self.center = self.center / poly.loop_total # average to get center of face         
         if not self.normal :
             raise RuntimeError("Unable to compute a normal for a face of \"%s\"." % (target.name,)) # degenerate geometry of some kind    
-        print("  Face normal: (%1.4f,%1.4f,%1.4f)" % (self.normal[0],self.normal[1],self.normal[2]))   
+        print("  Face normal: (%1.4f,%1.4f,%1.4f)" % (self.normal[0],self.normal[1],self.normal[2])) 
+        #   Compute bounding box of face.  Use base edge as the bottom of the bounding box.
+        #   This will be the area of the image we will take and map onto the face.  
+        #   ***MORE***
         
     def getedgeids(self) :
         """
         Return set of edge tuples for this face
         """
         return self.edgeids                    
-            
-        
-    def merge(self, otherface) :            # merge in another face
-        if (not self.normal) or (not otherface.normal) :
-            return False                    # merge fails
-        if self.normal.dot(otherface.normal) < (1.0 - NORMALERROR) :
-            return False                    # not coplanar, no merge
-        commonedges = self.edgeids.intersection(otherface.edgeids) 
-        if not commonedges :
-            return False                    # must have a common edge
-        self.polys.extend(otherface.polys)      # merge
-        self.edgeids = self.edgeids.union(otherface.edgeids)  
-        self.edgeids = self.edgeids.difference(commonedges)         # remove edges we just merged out
-        self.vertexids = self.vertexids.union(otherface.vertexids)  # Not too useful - order has been lost  
-        return True
-        
+                    
     def dump(self) :
-        print("Face: %d polys, %d vertices, normal (%1.4f,%1.4f,%1.4f)" %  (len(self.polys), len(self.vertexids), self.normal[0],self.normal[1],self.normal[2]))  
+        """
+        Debug output
+        """
+        print("Face:  %d vertices, normal (%1.4f,%1.4f,%1.4f), center (%1.4f,%1.4f,%1.4f)" % 
+            (len(self.vertexids), self.normal[0], self.normal[1], self.normal[2], self.center[0], self.center[1], self.center[2])) 
     
 
 
@@ -107,55 +109,32 @@ class ImpostorMaker(bpy.types.Operator) :
         pass
     
     def errormsg(self, msg) :
-        print("ERROR: " + msg)           # ***TEMP*** until we find a better way
+        print("ERROR: " + msg)          # ***TEMP*** until we find a better way
 
     def execute(self, context):         # execute() is called by blender when running the operator.
-        #   Do the work here
-        print("Impostor maker starting.")           # ***TEMP***
-        #   Target impostor is last selected object.
-        #   Objects to render are all other selected objects.
-        #   If only one object is selected, it is the impostor, 
-        #   and all other visible objects will be rendered.
+        """
+        Target impostor is last selected object.
+        Objects to render are all other selected objects.
+        If only one object is selected, it is the impostor, 
+        and all other visible objects will be rendered.
+        """
         if not context.selected_objects :
             self.errormsg("Nothing selected.")
             return {'FINISHED'}
-        target = context.selected_objects[-1]   # target impostor
+        target = context.selected_objects[-1]       # target impostor
         if target.type != 'MESH' :
             self.errormsg("Impostor must be mesh.")
             return {'FINISHED'}
-        sources = context.selected_objects[:-1] # source objects
-        if not sources :                # if no source objects
+        sources = context.selected_objects[:-1]     # source objects
+        if not sources :                            # if no source objects
             sources = [obj for obj in context.visible_objects if obj != target] # everything but target
         sources = [obj for obj in sources if obj.type in DRAWABLE]  # only drawables
         if not sources :
             self.errormsg("Nothing drawable to draw on the impostor.")
             return {'FINISHED'}
         self.buildimpostor(context, target, sources)
-        return {'FINISHED'}             # this lets blender know the operator finished successfully.
-        
-    def mergefaces(self, faces, target) :
-        """
-        Merge faces if they have the same normal and an edge in common.
-        """
-        #   Build list of which edges are in which faces
-        edgeusage = {}
-        for face in faces :
-            for edge in face.getedgeids() :
-                if edge in edgeusage :
-                    edgeusage[edge].append(face) 
-                else :
-                    edgeusage[edge] = [face]
-        #   Try to merge faces which share an edge
-        faceset = set(faces)
-        for edge, faces in edgeusage.items() :
-            if len(faces) > 2  or len(faces) < 1:
-                #   ***NEEDS TO BE SMARTER ABOUT MORE THAN 2 EDGES, which can happen after merging***
-                raise RuntimeError("Bad geometry: %d faces of \"%s\" share an edge." % (len(faces),target.name)) # degenerate geometry of some kind
-            if (len(faces) == 2) :                              # attempt merge
-                if faces[0].merge(faces[1]) and faces[1] in faceset :
-                    faceset.remove(faces[1])                    # if merge successful, drop one merged out from set
-        return list(faceset)
-        
+        return {'FINISHED'}                         # this lets blender know the operator finished successfully.
+                
     def limiteddissolve(self, context, target) :
         """
         Do a limited dissolve on the target object to combine coplanar triangles into big faces.
